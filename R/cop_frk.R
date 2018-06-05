f61181d0e143eb5e87b8a6976dbf3697981b7bbb
Vincenzo Coia <vincenzo.coia@gmail.com>
2018-06-04 22:11:10 -0700

    add a couple copula objects

---
+++ R/cop_frk.R
@@ -0,0 +1,47 @@
+#' Built-In Bivariate Parametric Copula Families
+#'
+#' These objects contain information about bivariate
+#' parametric copula families.
+#'
+#' @aliases cop_frk, cop_gum
+#' @details
+#' Each is a list containing the following components:
+#' \describe{
+#'   \item{pcop}{cdf}
+#'   \item{dcop}{pdf}
+#'   \item{logdcop}{log pdf}
+#'   \item{pcondcop}{list of length 2 containing the conditional distribution
+#'   functions of the first variable given the second, and the second variable
+#'   given the first (respectively).}
+#' }
+#' If symmetries are relevant, the above applies to
+#' direction=1, major_axis=1, and minor_axis=1 (whichever are relevant).
+#'
+#' These objects rely heavily on functions from the package CopulaModel,
+#' hence why coperate Depends on this package.
+NULL
+
+cop_frk <- list(
+    pcop      = pfrk,
+    dcop      = dfrk,
+    logdcop   = logdfrk,
+    pcondcop  = list(pcondfrk, pcondfrk),
+    qcondcop  = list(qcondfrk, qcondfrk),
+    cparspace = data.frame(theta=c(-Inf, Inf)),
+    symmspace = data.frame(direction  = c(1, -1),
+                           major_axis = 0,
+                           minor_axis = 0)
+)
+
+cop_gum <- list(
+    pcop      = pgum,
+    dcop      = dgum,
+    logdcop   = logdgum,
+    pcondcop  = list(pcondgum, pcondgum),
+    qcondcop  = list(qcondgum, qcondgum),
+    cparspace = data.frame(theta=c(-Inf, Inf)),
+    symmspace = expand.grid(direction  = c(1, -1),
+                            major_axis = c(1, -1),
+                            minor_axis = 0)
+)
+
