fc997d2cb761bdaa003aa188db5785949b88cd82
Vincenzo Coia <vincenzo.coia@gmail.com>
2018-08-03 16:07:56 -0700

    start intention for cpar restriction

--- R/restrict.R
+++ R/restrict.R
@@ -1,6 +1,13 @@
 #' Restrict a copula family
+#'
+#' @details
+#' Other identifiers you can specify besides those listed in the arguments:
+#'
+#' - \code{cpar}, corresponding to the canonical copula parameters. For
+#'    example, theta < 10.
+#'
 #' @export
-restrict <- function(cop, dependence=NULL) {
+restrict <- function(cop, dependence=NULL, ...) {
     if (!is.null(dependence)) {
         if (tolower(dependence)=="positive") {
             cop$symmspace <- subset(cop$symmspace, dependence==1)
@@ -8,5 +15,8 @@ restrict <- function(cop, dependence=NULL) {
             cop$symmspace <- subset(cop$symmspace, dependence==-1)
         }
     }
-    cop
+    # If cpar is in elipses:
+    cop$cparspace
 }
+
+use_vignette("copula_families")
